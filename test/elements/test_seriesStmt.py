fec0ad62d4de13e32b05f9eed21d6ab717d341f5
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-13 09:55:49 +0200

    [fix]: fixed tests for seriesStmt and related elements.

--- test/elements/test_seriesStmt.py
+++ test/elements/test_seriesStmt.py
@@ -9,51 +9,51 @@ from ..conftest import RNG_test_function
         (
             "valid-seriesStmt",
             """<seriesStmt xml:id='ssrq-sds-fds'>
-                <title>Sammlung Schweizerischer Rechtsquellen</title>
-                <respStmt>
-                    <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
-                    <resp key='publisher'/>
-                </respStmt>
-                <idno>SSRQ-FR-I_2_8-1-1</idno>
-                <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
-            </seriesStmt>""",
+                    <title>Sammlung Schweizerischer Rechtsquellen</title>
+                    <respStmt>
+                        <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
+                        <resp>Herausgeberschaft</resp>
+                    </respStmt>
+                    <idno>SSRQ-FR-I_2_8-1-1</idno>
+                    <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
+                </seriesStmt>""",
             True,
         ),
         (
             "invalid-seriesStmt-with-missing-title",
             """<seriesStmt xml:id='ssrq-sds-fds'>
-                <respStmt>
-                    <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
-                    <resp key='publisher'/>
-                </respStmt>
-                <idno>SSRQ-FR-I_2_8-1-1</idno>
-                <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
-            </seriesStmt>""",
+                    <respStmt>
+                        <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
+                        <resp>Herausgeberschaft</resp>
+                    </respStmt>
+                    <idno>SSRQ-FR-I_2_8-1-1</idno>
+                    <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
+                </seriesStmt>""",
             False,
         ),
         (
             "invalid-seriesStmt-with-one-idno",
             """<seriesStmt xml:id='ssrq-sds-fds'>
-                <title>Sammlung Schweizerischer Rechtsquellen</title>
-                <respStmt>
-                    <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
-                    <resp key='publisher'/>
-                </respStmt>
-                <idno>SSRQ-FR-I_2_8-1-1</idno>
-            </seriesStmt>""",
+                    <title>Sammlung Schweizerischer Rechtsquellen</title>
+                    <respStmt>
+                        <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
+                        <resp>Herausgeberschaft</resp>
+                    </respStmt>
+                    <idno>SSRQ-FR-I_2_8-1-1</idno>
+                </seriesStmt>""",
             False,
         ),
         (
             "invalid-seriesStmt-with-wrong-xml-id",
             """<seriesStmt xml:id='foo'>
-                <title>Sammlung Schweizerischer Rechtsquellen</title>
-                <respStmt>
-                    <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
-                    <resp key='publisher'/>
-                </respStmt>
-                <idno>SSRQ-FR-I_2_8-1-1</idno>
-                <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
-            </seriesStmt>""",
+                    <title>Sammlung Schweizerischer Rechtsquellen</title>
+                    <respStmt>
+                        <orgName>Rechtsquellenstiftung des Schweizerischen Juristenvereins</orgName>
+                        <resp>Herausgeberschaft</resp>
+                    </respStmt>
+                    <idno>SSRQ-FR-I_2_8-1-1</idno>
+                    <idno type='uuid'>ad28656b-5c8d-459c-afb4-3e6ddf70810d</idno>
+                </seriesStmt>""",
             False,
         ),
     ],
