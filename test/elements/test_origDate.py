00e4e2ff4708e839c8cba87ef09463823bec2b25
Bpolitycki <bastian.politycki@unisg.ch>
2023-06-13 09:55:10 +0200

    [fix/test]: attribute usage combined with @calendar

--- test/elements/test_origDate.py
+++ test/elements/test_origDate.py
@@ -1,6 +1,10 @@
 import pytest
+from pyschval.main import (
+    SchematronResult,
+    validate_chunk,
+)
 
-from ..conftest import RNG_test_function
+from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
 
 
 @pytest.mark.parametrize(
@@ -10,6 +14,11 @@ from ..conftest import RNG_test_function
             "valid-origDate",
             "<origDate when-custom='1448-05-25' calendar='gregorian'>bar</origDate>",
             True,
+        ),
+        (
+            "origDate-with-invalid-calendar",
+            "<origDate when-custom='1448-05-25' calendar='...'>bar</origDate>",
+            False,
         ),
         (
             "invalid-origDate-without-calendar",
@@ -30,3 +39,28 @@ def test_element(
     result: bool,
 ):
     test_element_with_rng("origDate", name, markup, result, False)
+
+
+@pytest.mark.parametrize(
+    "name, markup, result",
+    [
+        (
+            "valid-origDate",
+            "<origDate when-custom='1448-05-25' calendar='gregorian'>bar</origDate>",
+            True,
+        ),
+        (
+            "invalid-origDate-with-calendar-only",
+            "<origDate calendar='gregorian'>bar</origDate>",
+            False,
+        ),
+    ],
+)
+def test_origDate_datable_constraints(
+    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
+):
+    writer.write(name, add_tei_namespace(markup))
+    reports: list[SchematronResult] = validate_chunk(
+        files=writer.list(), isosch=main_constraints
+    )
+    assert reports[0].is_valid() is result
