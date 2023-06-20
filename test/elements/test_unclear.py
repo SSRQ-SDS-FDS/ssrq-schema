9b1933662341338422f4164034a299e19027671e
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-20 13:44:11 +0200

    [fix]: added non-empty-constraint to tei:foreign, tei:seg, tei:signed and tei:unclear

--- test/elements/test_unclear.py
+++ test/elements/test_unclear.py
@@ -1,6 +1,10 @@
 import pytest
+from pyschval.main import (
+    SchematronResult,
+    validate_chunk,
+)
 
-from ..conftest import RNG_test_function
+from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
 
 
 @pytest.mark.parametrize(
@@ -23,10 +27,30 @@ from ..conftest import RNG_test_function
         ),
     ],
 )
-def test_element(
+def test_unclear(
     test_element_with_rng: RNG_test_function,
     name: str,
     markup: str,
     result: bool,
 ):
     test_element_with_rng("unclear", name, markup, result, False)
+
+
+@pytest.mark.parametrize(
+    "name, markup, result",
+    [
+        (
+            "invalid-empty-unclear",
+            "<unclear/>",
+            False,
+        ),
+    ],
+)
+def test_unclear_constraints(
+    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
+):
+    writer.write(name, add_tei_namespace(markup))
+    reports: list[SchematronResult] = validate_chunk(
+        files=writer.list(), isosch=main_constraints
+    )
+    assert reports[0].is_valid() is result
