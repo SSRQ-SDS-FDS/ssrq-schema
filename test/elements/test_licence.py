9b1739751a6d8af3befe1775ef4bbf7fbaed8666
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-21 15:06:06 +0200

    [fix]: fixed content model and attribute for tei:licence

--- test/elements/test_licence.py
+++ test/elements/test_licence.py
@@ -8,7 +8,9 @@ import pytest
     [
         (
             "valid-licence",
-            "<licence target='http://licence.bar'>foo bar</licence>",
+            """<licence target='https://creativecommons.org/licenses/by-nc-sa/4.0/'>
+                    Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+                    </licence>""",
             True,
         ),
         (
