c28b5623e1f200eb82d9cf774f0d5498a8abb7dc
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-21 14:15:00 +0200

    [fix]: fixed content model and attributes for tei:witness

--- test/elements/test_listWit.py
+++ test/elements/test_listWit.py
@@ -8,13 +8,71 @@ import pytest
     [
         (
             "valid-listWit",
-            "<listWit><witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>Foo</witness></listWit>",
+            """<listWit>
+                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
+                            <msDesc>
+                                <head>foo</head>
+                                <physDesc>
+                                    <objectDesc form="book">
+                                        <supportDesc>
+                                            <support>
+                                                <material type="paper"/>
+                                            </support>
+                                        </supportDesc>
+                                    </objectDesc>
+                                </physDesc>
+                                <history>
+                                    <origin>
+                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                    </origin>
+                                </history>
+                            </msDesc>
+                        </witness>
+                        </listWit>""",
             True,
         ),
         (
             "valid-listWit-with-multiple-entries",
-            """<listWit><witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>Foo</witness>
-                    <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810e' n='B'>Bar</witness></listWit>""",
+            """<listWit>
+                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
+                            <msDesc>
+                                <head>foo</head>
+                                <physDesc>
+                                    <objectDesc form="book">
+                                        <supportDesc>
+                                            <support>
+                                                <material type="paper"/>
+                                            </support>
+                                        </supportDesc>
+                                    </objectDesc>
+                                </physDesc>
+                                <history>
+                                    <origin>
+                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                    </origin>
+                                </history>
+                            </msDesc>
+                        </witness>
+                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810e' n='B'>
+                            <msDesc>
+                                <head>foo</head>
+                                <physDesc>
+                                    <objectDesc form="book">
+                                        <supportDesc>
+                                            <support>
+                                                <material type="paper"/>
+                                            </support>
+                                        </supportDesc>
+                                    </objectDesc>
+                                </physDesc>
+                                <history>
+                                    <origin>
+                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                    </origin>
+                                </history>
+                            </msDesc>
+                        </witness>
+                    </listWit>""",
             True,
         ),
         (
@@ -24,7 +82,27 @@ import pytest
         ),
         (
             "invalid-listWit-with-attribute",
-            "<listWit type='foo'><witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>Foo</witness></listWit>",
+            """<listWit type='foo'>
+                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
+                            <msDesc>
+                                <head>foo</head>
+                                <physDesc>
+                                    <objectDesc form="book">
+                                        <supportDesc>
+                                            <support>
+                                                <material type="paper"/>
+                                            </support>
+                                        </supportDesc>
+                                    </objectDesc>
+                                </physDesc>
+                                <history>
+                                    <origin>
+                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                    </origin>
+                                </history>
+                            </msDesc>
+                        </witness>
+                    </listWit>""",
             False,
         ),
     ],
