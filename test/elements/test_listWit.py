83e6c0119889dc359c5daaf1e492f253889a22a2
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-23 13:25:57 +0200

    [fix]: fixed xml:id for tei:witness

--- test/elements/test_listWit.py
+++ test/elements/test_listWit.py
@@ -9,70 +9,70 @@ import pytest
         (
             "valid-listWit",
             """<listWit>
-                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
-                            <msDesc>
-                                <head>foo</head>
-                                <physDesc>
-                                    <objectDesc form="book">
-                                        <supportDesc>
-                                            <support>
-                                                <material type="paper"/>
-                                            </support>
-                                        </supportDesc>
-                                    </objectDesc>
-                                </physDesc>
-                                <history>
-                                    <origin>
-                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
-                                    </origin>
-                                </history>
-                            </msDesc>
-                        </witness>
-                        </listWit>""",
+                            <witness xml:id='id-ssrq-00000000-0000-4000-8000-000000000000' n='A'>
+                                <msDesc>
+                                    <head>foo</head>
+                                    <physDesc>
+                                        <objectDesc form="book">
+                                            <supportDesc>
+                                                <support>
+                                                    <material type="paper"/>
+                                                </support>
+                                            </supportDesc>
+                                        </objectDesc>
+                                    </physDesc>
+                                    <history>
+                                        <origin>
+                                            <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                        </origin>
+                                    </history>
+                                </msDesc>
+                            </witness>
+                            </listWit>""",
             True,
         ),
         (
             "valid-listWit-with-multiple-entries",
             """<listWit>
-                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
-                            <msDesc>
-                                <head>foo</head>
-                                <physDesc>
-                                    <objectDesc form="book">
-                                        <supportDesc>
-                                            <support>
-                                                <material type="paper"/>
-                                            </support>
-                                        </supportDesc>
-                                    </objectDesc>
-                                </physDesc>
-                                <history>
-                                    <origin>
-                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
-                                    </origin>
-                                </history>
-                            </msDesc>
-                        </witness>
-                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810e' n='B'>
-                            <msDesc>
-                                <head>foo</head>
-                                <physDesc>
-                                    <objectDesc form="book">
-                                        <supportDesc>
-                                            <support>
-                                                <material type="paper"/>
-                                            </support>
-                                        </supportDesc>
-                                    </objectDesc>
-                                </physDesc>
-                                <history>
-                                    <origin>
-                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
-                                    </origin>
-                                </history>
-                            </msDesc>
-                        </witness>
-                    </listWit>""",
+                            <witness xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
+                                <msDesc>
+                                    <head>foo</head>
+                                    <physDesc>
+                                        <objectDesc form="book">
+                                            <supportDesc>
+                                                <support>
+                                                    <material type="paper"/>
+                                                </support>
+                                            </supportDesc>
+                                        </objectDesc>
+                                    </physDesc>
+                                    <history>
+                                        <origin>
+                                            <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                        </origin>
+                                    </history>
+                                </msDesc>
+                            </witness>
+                            <witness xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810e' n='B'>
+                                <msDesc>
+                                    <head>foo</head>
+                                    <physDesc>
+                                        <objectDesc form="book">
+                                            <supportDesc>
+                                                <support>
+                                                    <material type="paper"/>
+                                                </support>
+                                            </supportDesc>
+                                        </objectDesc>
+                                    </physDesc>
+                                    <history>
+                                        <origin>
+                                            <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                        </origin>
+                                    </history>
+                                </msDesc>
+                            </witness>
+                        </listWit>""",
             True,
         ),
         (
@@ -83,26 +83,26 @@ import pytest
         (
             "invalid-listWit-with-attribute",
             """<listWit type='foo'>
-                        <witness xml:id='ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
-                            <msDesc>
-                                <head>foo</head>
-                                <physDesc>
-                                    <objectDesc form="book">
-                                        <supportDesc>
-                                            <support>
-                                                <material type="paper"/>
-                                            </support>
-                                        </supportDesc>
-                                    </objectDesc>
-                                </physDesc>
-                                <history>
-                                    <origin>
-                                        <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
-                                    </origin>
-                                </history>
-                            </msDesc>
-                        </witness>
-                    </listWit>""",
+                            <witness xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d' n='A'>
+                                <msDesc>
+                                    <head>foo</head>
+                                    <physDesc>
+                                        <objectDesc form="book">
+                                            <supportDesc>
+                                                <support>
+                                                    <material type="paper"/>
+                                                </support>
+                                            </supportDesc>
+                                        </objectDesc>
+                                    </physDesc>
+                                    <history>
+                                        <origin>
+                                            <origDate calendar="unknown" from-custom="1000-01-01" to-custom="1099-12-31">11. Jahrhundert</origDate>
+                                        </origin>
+                                    </history>
+                                </msDesc>
+                            </witness>
+                        </listWit>""",
             False,
         ),
     ],
