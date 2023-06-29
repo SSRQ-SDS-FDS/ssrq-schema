c825bbeddd4c1f4c07a0b1301e5f1752a4a3e9e7
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-06-29 17:34:44 +0200

    [refac]: refactored datatype definitions - changed order of definitions - added and updated translations - changed formatting

--- test/elements/test_titleStmt.py
+++ test/elements/test_titleStmt.py
@@ -9,67 +9,67 @@ import pytest
         (
             "valid-titleStmt",
             """<titleStmt>
-                <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
-                    Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
-                <editor>
-                    <persName>Ariane Huber Hernández</persName>
-                </editor>
-                <editor>
-                    <persName>Michael Nadig</persName>
-                </editor>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>Transkription</resp>
-                </respStmt>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>XML-Codierung des Transkripts</resp>
-                </respStmt>
-            </titleStmt>""",
+                    <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
+                        Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
+                    <editor>
+                        <persName>Ariane Huber Hernández</persName>
+                    </editor>
+                    <editor>
+                        <persName>Michael Nadig</persName>
+                    </editor>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>Transkription</resp>
+                    </respStmt>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>XML-Codierung der Transkription</resp>
+                    </respStmt>
+                </titleStmt>""",
             True,
         ),
         (
             "invalid-titleStmt-without-editor",
             """<titleStmt>
-                <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
-                    Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>Transkription</resp>
-                </respStmt>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>XML-Codierung des Transkripts</resp>
-                </respStmt>
-            </titleStmt>""",
+                    <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
+                        Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>Transkription</resp>
+                    </respStmt>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>XML-Codierung der Transkription</resp>
+                    </respStmt>
+                </titleStmt>""",
             False,
         ),
         (
             "invalid-titleStmt-with-attribute",
             """<titleStmt xml:id='bar'>
-                <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
-                    Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
-                <editor>
-                    <persName>Ariane Huber Hernández</persName>
-                </editor>
-                <editor>
-                    <persName>Michael Nadig</persName>
-                </editor>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>Transkription</resp>
-                </respStmt>
-                <respStmt>
-                    <persName>Ariane Huber Hernández</persName>
-                    <persName>Michael Nadig</persName>
-                    <resp>XML-Codierung des Transkripts</resp>
-                </respStmt>
-            </titleStmt>""",
+                    <title>I. Abteilung: Die Rechtsquellen des Kantons Zürich. Neue Folge. Zweiter Teil:
+                        Rechte der Landschaft. Band 11: Die Obervogteien um die Stadt Zürich</title>
+                    <editor>
+                        <persName>Ariane Huber Hernández</persName>
+                    </editor>
+                    <editor>
+                        <persName>Michael Nadig</persName>
+                    </editor>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>Transkription</resp>
+                    </respStmt>
+                    <respStmt>
+                        <persName>Ariane Huber Hernández</persName>
+                        <persName>Michael Nadig</persName>
+                        <resp>XML-Codierung der Transkription</resp>
+                    </respStmt>
+                </titleStmt>""",
             False,
         ),
     ],
