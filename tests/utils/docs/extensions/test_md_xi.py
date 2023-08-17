fa22019b60f3a68e938228cdf426758dc3acb798
Bpolitycki <bastian.politycki@unisg.ch>
2023-08-17 09:34:31 +0200

    [feat]: handler function to resolve xincludes from text input

--- tests/utils/docs/extensions/test_md_xi.py
+++ tests/utils/docs/extensions/test_md_xi.py
@@ -1,6 +1,24 @@
-import pytest
+from pathlib import Path
 
-from utils.docs.extensions.md_xi import XInclude, find_xi_includes
+import pytest
+from saxonche import (
+    PySaxonProcessor,
+)
+
+from utils.docs.extensions.md_xi import (
+    ResolvedXInclude,
+    XInclude,
+    find_element_by_xi_pointer,
+    find_xi_includes,
+    resolve_xi_includes,
+)
+
+
+@pytest.fixture(scope="function")
+def tmp_path_with_ex(tmp_path: Path) -> Path:
+    with open(tmp_path / "xi_ex.xml", "w") as ex:
+        ex.write("<seg xmlns='http://www.tei-c.org/ns/Examples'>world</seg>")
+    return tmp_path
 
 
 @pytest.mark.parametrize(
@@ -27,3 +45,104 @@ from utils.docs.extensions.md_xi import XInclude, find_xi_includes
 )
 def test_find_xi_includes(text: str, expected: list[XInclude] | None):
     assert find_xi_includes(text) == expected
+
+
+@pytest.mark.parametrize(
+    "xml, xi, expected",
+    [
+        ("<foo/>", XInclude(filename="bar.xml", xpointer=None), "<foo/>"),
+        (
+            """<div xmlns="http://www.tei-c.org/ns/1.0"><egXML xmlns="http://www.tei-c.org/ns/Examples" xml:id="ex-origDate-abbr">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr></origDate>
+            </egXML></div>""",
+            XInclude(filename="bar.xml", xpointer="ex-origDate-abbr"),
+            """<egXML xmlns="http://www.tei-c.org/ns/Examples">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr>
+            </origDate></egXML>""",
+        ),
+        (
+            """<div xmlns="http://www.tei-c.org/ns/1.0" xmlns:xi="http://www.w3.org/2001/XInclude"><egXML xmlns="http://www.tei-c.org/ns/Examples" xml:id="ex-origDate-abbr">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr></origDate>
+            <xi:include href="xi_ex.xml"/>
+            </egXML></div>""",
+            XInclude(filename="bar.xml", xpointer="ex-origDate-abbr"),
+            """<egXML xmlns="http://www.tei-c.org/ns/Examples">
+            <origDate  when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr>
+            </origDate><seg>world</seg></egXML>""",
+        ),
+    ],
+)
+def test_resolve_xi_includes(
+    tmp_path_with_ex: Path, xml: str, xi: XInclude, expected: str
+):
+    with open(tmp_path_with_ex / xi.filename, "w") as ex:
+        ex.write(xml)
+
+    results = list(
+        map(
+            lambda r: ResolvedXInclude(
+                include=r.include,
+                content=r.content.replace(
+                    'xmlns:xi="http://www.w3.org/2001/XInclude"',
+                    "",  # removing xi-namespaces, because they are irrelevant for testing
+                ),
+            ),
+            resolve_xi_includes(base_path=tmp_path_with_ex, includes=[xi]),
+        )
+    )
+
+    assert len(results) == 1
+
+    with PySaxonProcessor(license=False) as proc:
+        assert (
+            proc.parse_xml(xml_text=results[0].content).__str__()
+            == proc.parse_xml(xml_text=expected).__str__()
+        )
+
+
+@pytest.mark.parametrize(
+    "xml, xi, expected",
+    [
+        ("<foo/>", XInclude(filename="bar.xml", xpointer=None), "<foo/>"),
+        (
+            """<div xmlns="http://www.tei-c.org/ns/1.0"><egXML xmlns="http://www.tei-c.org/ns/Examples" xml:id="ex-origDate-abbr">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr></origDate>
+            </egXML></div>""",
+            XInclude(filename="bar.xml", xpointer="ex-origDate-abbr"),
+            """<egXML xmlns="http://www.tei-c.org/ns/Examples" xml:id="ex-origDate-abbr">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr>
+            </origDate></egXML>""",
+        ),
+    ],
+)
+def test_find_element_by_xi_pointer(xml: str, xi: XInclude, expected: str):
+    with PySaxonProcessor(license=False) as proc:
+        node = proc.parse_xml(xml_text=xml)
+        assert (
+            find_element_by_xi_pointer(saxon_proc=proc, node=node, include=xi).__str__()
+            == proc.parse_xml(
+                xml_text=expected
+            ).__str__()  # testing the equality of the string representation, not the objects itself (which are never equal)
+        )
+
+
+@pytest.mark.parametrize(
+    "xml, xi",
+    [
+        ("<foo/>", XInclude(filename="bar.xml", xpointer="bar")),
+        (
+            """<div xmlns="http://www.tei-c.org/ns/1.0"><egXML xmlns="http://www.tei-c.org/ns/Examples" xml:id="ex-origDate-abbr">
+            <origDate when-custom="--11-10" calendar="gregorian">10. <abbr>9bris</abbr></origDate>
+            </egXML></div>""",
+            XInclude(filename="bar.xml", xpointer="xyz"),
+        ),
+    ],
+)
+def test_find_element_by_xi_pointer_raises_error_for_unknown_id(
+    xml: str,
+    xi: XInclude,
+):
+    with pytest.raises(ValueError):
+        with PySaxonProcessor(license=False) as proc:
+            node = proc.parse_xml(xml_text=xml)
+            find_element_by_xi_pointer(saxon_proc=proc, node=node, include=xi)
--- utils/docs/extensions/md_xi.py
+++ utils/docs/extensions/md_xi.py
@@ -1,5 +1,16 @@
 import re
 from dataclasses import dataclass
+from pathlib import Path
+
+from saxonche import (
+    PySaxonProcessor,
+    PyXdmNode,
+    PyXPathProcessor,
+    PyXslt30Processor,
+    PyXsltExecutable,
+)
+
+import utils.commons.config as configs
 
 XI_PATTERN = re.compile(
     r"-+xi-include-+\s([A-Za-z0-9_\-]+\.[a-z]+)(?:#([A-Za-z0-9_\-]+))?"
@@ -10,6 +21,12 @@ XI_PATTERN = re.compile(
 class XInclude:
     filename: str
     xpointer: str | None
+
+
+@dataclass(frozen=True)
+class ResolvedXInclude:
+    include: XInclude
+    content: str
 
 
 def find_xi_includes(
@@ -34,3 +51,54 @@ def find_xi_includes(
         XInclude(filename=result[0], xpointer=result[1] if len(result[1]) > 0 else None)
         for result in results
     ]
+
+
+def resolve_xi_includes(
+    includes: list[XInclude], base_path: Path = configs.EXAMPLES_DIR.absolute()
+) -> list[ResolvedXInclude]:
+    resolved_includes: list[ResolvedXInclude] = []
+
+    with PySaxonProcessor(license=False) as proc:
+        xslt_proc: PyXslt30Processor = proc.new_xslt30_processor()
+
+        for include in includes:
+            document: PyXdmNode = find_element_by_xi_pointer(
+                saxon_proc=proc,
+                node=proc.parse_xml(xml_file_name=str(base_path / include.filename)),
+                include=include,
+            )
+
+            xslt_proc.set_parameter(  # type: ignore
+                "path_base", proc.make_string_value(base_path.as_uri())  # type: ignore
+            )
+
+            xsl: PyXsltExecutable = xslt_proc.compile_stylesheet(  # type: ignore
+                stylesheet_file=str(configs.XSLTS["xi"])
+            )
+            xslt_result: str = xsl.transform_to_string(xdm_node=document)
+
+            if xslt_result is None:
+                raise ValueError(f"Failed to resolve xincludes for {include.filename}")
+
+            resolved_includes.append(
+                ResolvedXInclude(include=include, content=xslt_result)
+            )
+
+    return resolved_includes
+
+
+def find_element_by_xi_pointer(
+    saxon_proc: PySaxonProcessor, node: PyXdmNode, include: XInclude
+) -> PyXdmNode:
+    if include.xpointer is None:
+        return node
+
+    xpath_proc: PyXPathProcessor = saxon_proc.new_xpath_processor()
+    xpath_proc.set_context(xdm_item=node)
+
+    if (
+        xi_query := xpath_proc.evaluate_single(f"//*[@xml:id = '{include.xpointer}']")
+    ) is not None:
+        return xi_query.get_node_value()
+
+    raise ValueError(f"Could not find the id {include.xpointer} in {include.filename}")
