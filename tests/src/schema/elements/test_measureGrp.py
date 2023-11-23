1a82ea96bf1fc607d31968b44f27aa19982fa50e
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-11-23 10:43:29 +0100

    fix: changed content model for tei:measureGrp
    This commit changes the content model for tei:measureGrp to allow the use of tei:cell as child elements. A schematron constraint ensures that at least two tei:measure-elements are within a tei:measureGrp.

--- tests/src/schema/elements/test_measureGrp.py
+++ tests/src/schema/elements/test_measureGrp.py
@@ -1,6 +1,10 @@
 import pytest
+from pyschval.main import (
+    SchematronResult,
+    validate_chunk,
+)
 
-from ..conftest import RNG_test_function
+from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
 
 
 @pytest.mark.parametrize(
@@ -10,6 +14,21 @@ from ..conftest import RNG_test_function
             "valid-measureGrp",
             "<measureGrp><lb/><measure quantity='5' unit='ß' type='currency'>fuͤnf schilling pfenig</measure> und <measure quantity='5' unit='ß' type='currency'>fuͤnf schilling pfenig</measure></measureGrp>",
             True,
+        ),
+        (
+            "valid-measureGrp-with-cells",
+            """<measureGrp>
+                    <cell><measure type="currency" quantity="2" unit="fl">11</measure></cell>
+                    <cell><measure type="currency" unit="xr" quantity="15">15</measure></cell>
+                </measureGrp>""",
+            True,
+        ),
+        (
+            "invalid-measureGrp-with-one-cell",
+            """<measureGrp>
+                    <cell><measure type="currency" quantity="2" unit="fl">11</measure></cell>
+                </measureGrp>""",
+            False,
         ),
         (
             "invalid-measureGrp-without-content",
@@ -35,3 +54,26 @@ def test_measureGrp(
     result: bool,
 ):
     test_element_with_rng("measureGrp", name, markup, result, False)
+
+
+@pytest.mark.parametrize(
+    "name, markup, result",
+    [
+        (
+            "invalid-measureGrp-with-cells-but-without-measure",
+            """<measureGrp>
+                    <cell>foo</cell>
+                    <cell>foo</cell>
+                </measureGrp>""",
+            False,
+        ),
+    ],
+)
+def test_measureGrp_constraints(
+    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
+):
+    writer.write(name, add_tei_namespace(markup))
+    reports: list[SchematronResult] = validate_chunk(
+        files=writer.list(), isosch=main_constraints
+    )
+    assert reports[0].is_valid() is result
