048605b7b53f904a7676f4da54f975621a85d27f
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-12-15 16:13:55 +0100

    refac: reformatting according to ruff
    This commit reformats some files according to a new rule in ruff.

--- tests/src/schema/elements/conftest.py
+++ tests/src/schema/elements/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture
 def element_schema(
-    change_rng_start_per_odd: dict[str, dict[str, str]]
+    change_rng_start_per_odd: dict[str, dict[str, str]],
 ) -> dict[str, str]:
     """A fixture, which returns the main ssrq schema with a modified start element."""
     return change_rng_start_per_odd["TEI_Schema"]
