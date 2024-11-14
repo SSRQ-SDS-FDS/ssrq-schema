27309667e86f8a56a3d2e3f5fb3a31799e779416
ChristianSonderUniSG <christian.sonder@unisg.ch>
2024-11-14 16:08:35 +0100

    test and formatting for tei:ab

--- tests/src/schema/elements/test_ab.py
+++ tests/src/schema/elements/test_ab.py
@@ -1,36 +1,59 @@
 import pytest
-from pyschval.schematron.validate import apply_schematron_validation
-from pyschval.types.result import SchematronResult
 
-from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
+from ..conftest import RNG_test_function
 
 
 @pytest.mark.parametrize(
     "name, markup, result",
     [
         (
-            "invalid-ab-with-scribe",
-            "<ab type='dorsal' place='cover' scribe='per011353'><lb/>Copiert - 976 fol</ab>",
-            False,
-        ),
-        (
-            "valid-ab-with-place-and-type",
-            "<ab type='dorsal' place='cover'><lb/>Copiert - 976 fol</ab>",
+            "valid-ab-with-place-and-type-text-content",
+            "<ab type='dorsal' place='left_margin'>Foo</ab>",
             True,
         ),
         (
-            "valid-ab-with-rend",
-            "<ab type='dorsal' place='cover' rend='pencil'><lb/>Copiert - 976 fol</ab>",
-            True,
-        ),
-        (
-            "valid-ab-with-hand",
-            " <ab type='archiving_reference' place='left_margin' hand='hand20cf'>St. Georgenamt FC 2</ab>",
+            "valid-ab-with-place-and-type-default-content",
+            "<ab type='dorsal' place='left_margin'><persName>Foo</persName> bar</ab>",
             True,
         ),
         (
             "invalid-ab-without-type",
-            " <ab place='left_margin' hand='hand20c'>St. Georgenamt FC 2</ab>",
+            " <ab place='left_margin'>Foo</ab>",
+            False,
+        ),
+        (
+            "invalid-ab-with-wrong-type",
+            " <ab type='foo' place='left_margin'>Foo</ab>",
+            False,
+        ),
+        (
+            "invalid-ab-without-place",
+            " <ab type='dorsal'>Foo</ab>",
+            False,
+        ),
+        (
+            "invalid-ab-with-wrong-place",
+            " <ab type='dorsal' place='foo'>Foo</ab>",
+            False,
+        ),
+        (
+            "valid-ab-with-hand",
+            " <ab type='dorsal' place='left_margin' hand='hand20cf'>Foo</ab>",
+            True,
+        ),
+        (
+            "valid-ab-with-rend",
+            "<ab type='dorsal' place='left_margin' rend='pencil'>Foo</ab>",
+            True,
+        ),
+        (
+            "valid-ab-with-xml-lang",
+            "<ab type='dorsal' place='left_margin' xml:lang='de'>Foo</ab>",
+            True,
+        ),
+        (
+            "invalid-ab-with-wrong-attribute",
+            "<ab type='dorsal' place='left_margin' scribe='per011353'>Foo</ab>",
             False,
         ),
     ],
@@ -42,23 +65,3 @@ def test_ab_rng(
     result: bool,
 ):
     test_element_with_rng("ab", name, markup, result, False)
-
-
-@pytest.mark.parametrize(
-    "name, markup, result",
-    [
-        (
-            "invalid-empty-ab",
-            "<ab type='archiving_reference' place='left margin' hand='hand20cf'/>",
-            False,
-        ),
-    ],
-)
-def test_ab_constraints(
-    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
-):
-    writer.write(name, add_tei_namespace(markup))
-    reports: list[SchematronResult] = apply_schematron_validation(
-        input=writer.list(), isosch=main_constraints
-    )
-    assert reports[0].report.is_valid() is result
