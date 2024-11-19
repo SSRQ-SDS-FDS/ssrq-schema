badbf34d5265a512ff6812fb543321302edcfc50
ChristianSonderUniSG <christian.sonder@unisg.ch>
2024-11-19 15:04:26 +0100

    feat: removed @period from custEvent

--- tests/src/schema/elements/test_custEvent.py
+++ tests/src/schema/elements/test_custEvent.py
@@ -8,22 +8,43 @@ from ..conftest import RNG_test_function
     [
         (
             "valid-custEvent",
-            """<custEvent type="lost" notBefore-custom="1858-09-01" calendar="gregorian">Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.</custEvent>""",
+            """
+            <custEvent type="lost" notBefore-custom="1858-09-01" calendar="gregorian">
+                Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.
+            </custEvent>
+            """,
             True,
         ),
         (
+            "invalid-custEvent-with-period",
+            """
+            <custEvent type="lost" period="winter" calendar="gregorian">
+                Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.
+            </custEvent>
+            """,
+            False,
+        ),
+        (
             "invalid-custEvent-type",
-            """<custEvent type="found_again" notBefore-custom="1858-09-01" calendar="gregorian">Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.</custEvent>""",
+            """
+            <custEvent type="found_again" notBefore-custom="1858-09-01" calendar="gregorian">
+                Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.
+            </custEvent>
+            """,
             False,
         ),
         (
             "invalid-custEvent-without-type",
-            """<custEvent notBefore-custom="1858-09-01" calendar="gregorian">Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.</custEvent>""",
+            """
+            <custEvent notBefore-custom="1858-09-01" calendar="gregorian">
+                Verlust nicht vor Erscheinen der Erstedition des 19. Jahrhunderts.
+            </custEvent>
+            """,
             False,
         ),
     ],
 )
-def test_custEvent(
+def test_cust_event(
     test_element_with_rng: RNG_test_function,
     name: str,
     markup: str,
