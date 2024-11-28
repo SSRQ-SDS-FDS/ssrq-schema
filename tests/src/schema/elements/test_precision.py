860062b942edbb28ca2dcddb8f017ccfbc4b047c
ChristianSonderUniSG <christian.sonder@unisg.ch>
2024-11-28 16:58:37 +0100

    feat: tests for precision

--- tests/src/schema/elements/test_precision.py
+++ tests/src/schema/elements/test_precision.py
@@ -9,28 +9,23 @@ from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
     "name, markup, result",
     [
         (
+            "invalid-precision-with-content",
+            "<precision match='@when-custom' precision='low'>foo</precision>",
+            False,
+        ),
+        (
             "valid-precision",
             "<precision precision='low' match='@to-custom'/>",
             True,
         ),
         (
-            "valid-precision",
+            "valid-precision-with-multiple-matches",
             "<precision precision='low' match='@notBefore-custom @notAfter-custom'/>",
-            True,
-        ),
-        (
-            "valid-precision-with-multiple-match-values",
-            "<precision precision='low' match='@from-custom @to-custom'/>",
             True,
         ),
         (
             "valid-precision-with-invalid-match-value",
             "<precision precision='low' match='@from-custom@to-custom'/>",
-            False,
-        ),
-        (
-            "valid-precision-with-invalid-precision",
-            "<precision precision='0.5' match='@to-custom'/>",
             False,
         ),
         (
@@ -59,22 +54,41 @@ def test_precision(
     [
         (
             "valid-precision-match",
-            "<date calendar='gregorian' when-custom='1637-10-18'>1637 [Oktober 18]<precision precision='low' match='@when-custom'/></date>",
+            """
+            <date calendar='gregorian' when-custom='1637-10-18'>
+                1637 [Oktober 18]
+                <precision precision='low' match='@when-custom'/>
+            </date>
+            """,
             True,
         ),
         (
             "multiple-valid-precision-match",
-            "<date calendar='gregorian' notBefore-custom='1637' notAfter-custom='1703'>nach 1638<precision precision='high' match='@notBefore-custom @notAfter-custom'/></date>",
+            """
+            <date calendar='gregorian' notBefore-custom='1637' notAfter-custom='1703'>
+                nach 1638
+                <precision precision='high' match='@notBefore-custom @notAfter-custom'/>
+            </date>
+            """,
             True,
         ),
         (
             "invalid-precision-match",
-            "<date calendar='gregorian' when-custom='1637-10-18'>1637 [Oktober 18]<precision precision='low' match='@when-customs'/></date>",
+            """
+            <date calendar='gregorian' when-custom='1637-10-18'>
+                1637 [Oktober 18]
+                <precision precision='low' match='@when-customs'/>
+            </date>""",
             False,
         ),
         (
             "invalid-multiple-valid-precision-match",
-            "<date calendar='gregorian' notBefore-custom='1637' notAfter-custom='1703'>nach 1638<precision precision='high' match='@notBefore-custom @notafter-custom'/></date>",
+            """
+            <date calendar='gregorian' notBefore-custom='1637' notAfter-custom='1703'>
+                nach 1638
+                <precision precision='high' match='@notBefore-custom @notafter-custom'/>
+            </date>
+            """,
             False,
         ),
     ],
