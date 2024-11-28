b7c34b9677bb57b10b12a49143d757eb8a9d1fbd
ChristianSonderUniSG <christian.sonder@unisg.ch>
2024-11-28 16:43:20 +0100

    feat: tests for placeName

--- tests/src/schema/elements/test_placeName.py
+++ tests/src/schema/elements/test_placeName.py
@@ -8,17 +8,27 @@ from ..conftest import RNG_test_function
     [
         (
             "valid-placeName",
-            "<placeName>bar</placeName>",
+            "<placeName>foo</placeName>",
             True,
         ),
         (
+            "valid-placeName-with-content-default",
+            "<placeName><del>foo</del> bar</placeName>",
+            True,
+        ),
+        (
+            "invalid-placeName-with-wrong-content",
+            "<placeName><p>foo</p></placeName>",
+            False,
+        ),
+        (
             "valid-placeName-with-ref",
-            "<placeName ref='loc000001'>bar</placeName>",
+            "<placeName ref='loc000001'>foo</placeName>",
             True,
         ),
     ],
 )
-def test_placeName(
+def test_place_name(
     test_element_with_rng: RNG_test_function,
     name: str,
     markup: str,
