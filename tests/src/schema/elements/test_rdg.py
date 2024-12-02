1a0a3324f120539a9dd4a5c931b94dba02f83923
ChristianSonderUniSG <christian.sonder@unisg.ch>
2024-12-02 12:12:08 +0100

    feat: tests for rdg

--- tests/src/schema/elements/test_rdg.py
+++ tests/src/schema/elements/test_rdg.py
@@ -1,8 +1,6 @@
 import pytest
-from pyschval.schematron.validate import apply_schematron_validation
-from pyschval.types.result import SchematronResult
 
-from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
+from ..conftest import RNG_test_function
 
 
 @pytest.mark.parametrize(
@@ -10,18 +8,13 @@ from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
     [
         (
             "valid-rdg",
-            "<rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810e#fol30r'>bar</rdg>",
+            "<rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'>foo</rdg>",
             True,
         ),
         (
-            "rdg-with-invalid-xml-id-reference",
-            "<rdg wit='baz id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810e'>bar</rdg>",
-            False,
-        ),
-        (
-            "invalid-rdg-wit-false-attribute",
-            "<rdg type='foo' wit='ad28656b-5c8d-459c-afb4-3e6ddf70810d'>bar</rdg>",
-            False,
+            "valid-empty-rdg",
+            "<rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'/>",
+            True,
         ),
         (
             "invalid-rdg-without-attribute",
@@ -39,19 +32,18 @@ from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
                       id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#n123-234
                       id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123r
                       id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123v
-                      id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123r-124v">bar</rdg>""",
+                      id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123r-124v">bar</rdg>
+            """,
             True,
         ),
         (
             "invalid-rdg-with-incorrect-wit-value-1",
-            """
-            <rdg wit="id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#s123">bar</rdg>""",
+            "<rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#s123'>bar</rdg>",
             False,
         ),
         (
             "invalid-rdg-with-incorrect-wit-value-2",
-            """
-            <rdg wit="id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123">bar</rdg>""",
+            "<rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d#fol123'>bar</rdg>",
             False,
         ),
     ],
@@ -63,35 +55,3 @@ def test_rdg(
     result: bool,
 ):
     test_element_with_rng("rdg", name, markup, result, False)
-
-
-@pytest.mark.parametrize(
-    "name, markup, result",
-    [
-        (
-            "valid-wit-references",
-            """<TEI>
-                <witness xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'/>
-                <bibl xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810e'>foo</bibl>
-                <rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810e#fol30r'>bar</rdg>
-            </TEI>""",
-            True,
-        ),
-        (
-            "invalid-wit-reference",
-            """<TEI>
-                <witness xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'/>
-                <rdg wit='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d baz'>bar</rdg>
-            </TEI>""",
-            False,
-        ),
-    ],
-)
-def test_witness_attr_constraint(
-    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
-):
-    writer.write(name, add_tei_namespace(markup))
-    reports: list[SchematronResult] = apply_schematron_validation(
-        input=writer.list(), isosch=main_constraints
-    )
-    assert reports[0].report.is_valid() is result
