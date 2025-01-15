276c0e9992869778b423cd5eb244e9bdfc4018ba
ChristianSonderUniSG <christian.sonder@unisg.ch>
2025-01-15 09:47:54 +0100

    feat: constraint for bibl/@xml:id
    bibl may have an xml:id only when inside listBibl

--- tests/src/schema/elements/test_bibl.py
+++ tests/src/schema/elements/test_bibl.py
@@ -1,6 +1,8 @@
 import pytest
+from pyschval.schematron.validate import apply_schematron_validation
+from pyschval.types.result import SchematronResult
 
-from ..conftest import RNG_test_function
+from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
 
 
 @pytest.mark.parametrize(
@@ -48,3 +50,32 @@ def test_bibl(
     result: bool,
 ):
     test_element_with_rng("bibl", name, markup, result, False)
+
+
+@pytest.mark.parametrize(
+    "name, markup, result",
+    [
+        (
+            "valid-bibl-with-xml-id-inside-listBibl",
+            """
+            <listBibl>
+                <bibl xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'>foo</bibl>
+            </listBibl>
+            """,
+            True,
+        ),
+        (
+            "invalid-bibl-with-xml-id-outside-listBibl",
+            "<note><bibl xml:id='id-ssrq-ad28656b-5c8d-459c-afb4-3e6ddf70810d'>foo</bibl></note>",
+            False,
+        ),
+    ],
+)
+def test_bibl_constraint(
+    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
+):
+    writer.write(name, add_tei_namespace(markup))
+    reports: list[SchematronResult] = apply_schematron_validation(
+        input=writer.list(), isosch=main_constraints
+    )
+    assert reports[0].report.is_valid() is result
