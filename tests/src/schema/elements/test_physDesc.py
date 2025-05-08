1b4646e7c660aa183e9fd0eac76043c1ee151e3f
Bpolitycki <bastian.politycki@unisg.ch>
2025-05-08 16:48:41 +0200

    feat: constraint to check for tei:bindingDesc
    Constraint is not restricted to texts with type 'collection', because
    the constraint for tei:msDesc ensures, that no tei:physDesc is used for
    a collection.

--- tests/src/schema/elements/test_physDesc.py
+++ tests/src/schema/elements/test_physDesc.py
@@ -1,6 +1,8 @@
 import pytest
+from pyschval.schematron.validate import apply_schematron_validation
+from pyschval.types.result import SchematronResult
 
-from ..conftest import RNG_test_function
+from ..conftest import RNG_test_function, SimpleTEIWriter, add_tei_namespace
 
 
 @pytest.mark.parametrize(
@@ -132,3 +134,58 @@ def test_phys_desc(
     result: bool,
 ):
     test_element_with_rng("physDesc", name, markup, result, False)
+
+
+@pytest.mark.parametrize(
+    "name, markup, result",
+    [
+        (
+            "valid-physDesc-with-bindingDesc",
+            """
+            <physDesc>
+                <objectDesc>
+                    <supportDesc>
+                        <support>
+                            <material type="paper"/>
+                        </support>
+                    </supportDesc>
+                </objectDesc>
+                <bindingDesc>
+                    <p>foo</p>
+                </bindingDesc>
+                <sealDesc>
+                    <seal condition="absent" n="1"/>
+                </sealDesc>
+            </physDesc>
+            """,
+            True,
+        ),
+        (
+            "invalid-physDesc-without-bindingDesc",
+            """
+            <physDesc>
+                <objectDesc>
+                    <supportDesc>
+                        <support>
+                            <material type="paper"/>
+                        </support>
+                    </supportDesc>
+                </objectDesc>
+                <sealDesc>
+                    <seal condition="absent" n="1"/>
+                </sealDesc>
+            </physDesc>
+            """,
+            False,
+        ),
+    ],
+)
+def test_phys_desc_constraints(
+    main_constraints: str, writer: SimpleTEIWriter, name: str, markup: str, result: bool
+):
+    writer.write(name, add_tei_namespace(markup))
+    reports: list[SchematronResult] = apply_schematron_validation(
+        input=writer.list(), isosch=main_constraints
+    )
+
+    assert reports[0].report.is_valid() is result
